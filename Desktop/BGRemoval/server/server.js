a44880a32f563504028c7cb4cc2ce7583a8b7a2f
Shrilakshmi Kamat <shrilakshmikamat@gamil.com>
2025-06-08 12:32:11 +0530

    Webhooks Added

--- Desktop/BGRemoval/server/server.js
+++ Desktop/BGRemoval/server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 
 const PORT= process.env.PORT||4000
@@ -12,5 +13,6 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/',(req,res)=>res.send("API Working"))
+app.use('/api/user',userRouter)
 
 app.listen(PORT,()=> console.log('Server running on port '+PORT ))
